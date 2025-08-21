4fb71412e2a6e09aeb76ecd40935fa69547a8e8e
rad_vrc <dorado1031@gmail.com>
2025-08-21 16:59:18 +0900

    s

--- TranslateTest2/obj/Debug/net8.0/TranslateTest2.AssemblyInfo.cs
+++ TranslateTest2/obj/Debug/net8.0/TranslateTest2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TranslateTest2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493d212195b67ab9f1374354f68bf9392a4f2f90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990324f8fb2dd5b6c957e3b6e5716ff72c4a28d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TranslateTest2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TranslateTest2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
